45c1dba02ccfe9bd84961a8804c6712d600b1ee4
AmZa <32337304+Amza99@users.noreply.github.com>
2020-12-03 11:49:57 +0100

    Add files via upload

--- IA-MaghrebUnited/Projet/Code/API.py
+++ IA-MaghrebUnited/Projet/Code/API.py
@@ -11,25 +11,48 @@ import DB_Connector as DB
 """
 http://localhost:5000/billets
 http://localhost:5000/users
+http://localhost:5000/info
 http://127.0.0.1:5000/users?Nom=NOM_TEST&Prenom=Prenom_TEST
-"""
+http://127.0.0.1:5000/billets?Nom=Prenom_TEST&Prenom=Prenom_TEST&Id_Vol=1
 
+"""
+    
+    
 class Billet(Resource):
-    def get(self):
+    def get(self):#get billet dispo
         Connection = DB.DATABASE()
         data = Connection.Get_Billet_Dispo()
         data = data.to_dict()
         Connection.Close()
         return {'data': data}, 200
+    def post(self):#Reservation
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('Nom', required=True)
+        parser.add_argument('Prenom', required=True)
+        parser.add_argument('Id_Vol', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+        Connection = DB.DATABASE()
+        if( Connection.Reservation_User_NP_ID_VOL(args['Nom'] ,  args['Prenom'] ,args['Id_Vol'] ) ==  -1 ):
+            return {'Status':'Vol indisponible'},200
+        Connection.Close()
+        return {'Status':'Done'},200
 
+class Info(Resource):
+    def get(self):# get all users reservation 
+        Connection = DB.DATABASE()
+        data = Connection.Get_ALL_Users_Reservation()
+        data = data.to_dict()
+        Connection.Close()
+        return {'data': data}, 200
+        
 class Users(Resource):
-    def get(self):
+    def get(self):# get list users
         Connection = DB.DATABASE()
         data = Connection.Get_All_Users()
         data = data.to_dict()
         Connection.Close()
         return {'data': data}, 200
-    def post(self):
+    def post(self):# create a user
         parser = reqparse.RequestParser()  # initialize
         parser.add_argument('Nom', required=True)
         parser.add_argument('Prenom', required=True)
@@ -37,7 +60,7 @@ class Users(Resource):
         Connection = DB.DATABASE()
         Connection.Insert_New_User(args['Nom'] ,args['Prenom'] )
         Connection.Close()
-        return {'Statut':'Done'},200
+        return {'Status':'Done'},200
 
 
 app = Flask(__name__)
@@ -45,7 +68,7 @@ api = Api(app)
 
 api.add_resource(Users, '/users')  
 api.add_resource(Billet, '/billets') 
-
+api.add_resource(Info, '/info')  
 
 
 app.run()
--- IA-MaghrebUnited/Projet/Code/DB_Connector.py
+++ IA-MaghrebUnited/Projet/Code/DB_Connector.py
@@ -19,22 +19,57 @@ class DATABASE:
     def Get_Number_of_Users(self):
         self.cursor.execute("SELECT COUNT(Id_Users) from Users;")
         return self.cursor.fetchall()[0][0]
+    def Reservation(self , Indx_Users , Indx_Vol ):
+        query = " INSERT INTO User_Reservation VALUES("+str(Indx_Users)+","+str(Indx_Vol)+")"
+        self.Run_Query(query)
     def Insert_New_User(self,Nom,Prenom):
         New_Index = self.Get_Number_of_Users()+1
         query = "INSERT INTO Users VALUES ("+str(New_Index)+", '"+Nom+"','"+Prenom+"')"
         self.Run_Query(query)
     def Get_Billet_Dispo(self):
-        query = """SELECT Id_Billet , AA.Nom_Airoport as Fromm , A.Nom_Airoport as too , Prix , Date(_Date_)  , NB_Billet_Dispo  FROM Billet as B
-                    INNER JOIN Airoport_ID as A on A.Id_Airoport = B.ID_Destination
-                    INNER JOIN Airoport_ID as AA on AA.Id_Airoport = B.ID_Provenance
-                    where NB_Billet_Dispo != 0
-                    """
+        query = """
+        SELECT Id_Billet , AA.Nom_Airoport as Fromm , A.Nom_Airoport as too , Prix , Date(_Date_)  , NB_Billet_Dispo  FROM Billet as B
+        INNER JOIN Airoport_ID as A on A.Id_Airoport = B.ID_Destination
+        INNER JOIN Airoport_ID as AA on AA.Id_Airoport = B.ID_Provenance
+        where NB_Billet_Dispo != 0
+        """
         self.cursor.execute(query)
         data = pandas.DataFrame(self.cursor.fetchall(),columns =["Id_Billet","From","To","Price","Date","Billet_Dispo"]) 
         return data
+    def Get_ALL_Users_Reservation(self):
+        query = """
+        SELECT Id_Billet ,U.Nom as NOM , U.Prenom as Prenom , AA.Nom_Airoport as Fromm , A.Nom_Airoport as too , Date(_Date_) FROM Billet as B
+        INNER JOIN Airoport_ID as A on A.Id_Airoport = B.ID_Destination
+        INNER JOIN Airoport_ID as AA on AA.Id_Airoport = B.ID_Provenance
+        INNER JOIN User_Reservation as UR on UR.Id_Vol = B.Id_Billet
+        INNER JOIN Users as U on U.Id_Users = UR.Id_User
+        """
+        self.cursor.execute(query)
+        data = pandas.DataFrame(self.cursor.fetchall(),columns =["Id_Billet","Nom","Prenom","From","To","Date"]) 
+        return data
+    def Get_ID_User(self,Nom,Prenom):
+        self.cursor.execute("SELECT Id_Users FROM Users as U where Nom like '"+Nom+"' and Prenom like '"+Prenom+"' ")
+        out  = self.cursor.fetchall()
+        if len( out ) == 0 :
+            return None
+        return out[0][0]
+    def Reservation_User_NP_ID_VOL(self,Nom,Prenom,Id_Vol):
+        # on test si le billet existe ou n'est pas dispo 
+        self.cursor.execute("SELECT * FROM Billet as B where NB_Billet_Dispo != 0 and Id_Billet like "+str(Id_Vol))
+        if len(self.cursor.fetchall()) == 0 :
+            return -1
+        if( self.Get_ID_User(Nom,Prenom) == None ):# si l'utilisateur n'existe pas on le creer 
+            self.Insert_New_User(Nom,Prenom)
+        Indx_User = self.Get_ID_User(Nom,Prenom)# on recupere son ID 
+        self.Reservation(Indx_User,Id_Vol)
+        #on decremente le nombre de billet 
+        self.Run_Query("Update Billet Set NB_Billet_Dispo=NB_Billet_Dispo-1 where Id_Billet like "+str(Id_Vol))
     def Run_Query(self , query):
         self.cursor.execute( query )
         self.connection.commit()
     def Close(self):
         self.cursor.close()
         self.connection.close()
+                
+        
+    
