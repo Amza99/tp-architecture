d4f451328ed9b9a637049a4e273e9a001836f654
AAMTAA <32337551+aamtaa@users.noreply.github.com>
2020-12-09 23:20:27 +0100

    100%

--- IA-MaghrebUnited/Projet/Code/User_Interface.py
+++ IA-MaghrebUnited/Projet/Code/User_Interface.py
@@ -9,20 +9,22 @@ class User :
         self.url = "http://localhost:5000/"
         self.Index_User = Index_User
         self.window = fenetre
-        self.window.geometry("790x200")
         self.window.title(' Maghreb_United')
         self.List_Vol_For_Reservation()
     def List_Vol_For_Reservation(self):
         self.Clean_Screen()
         Label(self.window, text="      Liste des Vols   ").grid(row=0 , column=0)
         Button(self.window,text='Mes Reservations',command= self.List_Deja_Reserver).grid(row=0,column=1,sticky=W,pady=4)
-        Label(self.window, text="      Depart     |").grid(row=1 , column=0)
-        Label(self.window, text="    Destination  |").grid(row=1, column=1)
-        Label(self.window, text="       Prix      |").grid(row=1, column=2)
-        Label(self.window, text="      Date       |").grid(row=1 , column=3)
-        Label(self.window, text="  Nombre de billet dispo ").grid(row=1 , column=4)
-        req = requests.get("http://localhost:5000/billets")
-        data  = pandas.DataFrame.from_dict(req.json()['data'] )
+        Field = ["      Depart     |","    Destination  |","       Prix      |","      Date       |","  Nombre de billet dispo "]
+        for i in range( len(Field )):
+            Label(self.window, text=Field[i]).grid(row=1 , column=i)
+        Input = {'Id_User': self.Index_User}
+        req = requests.get("http://localhost:5000/billets",data = Input)
+        try:
+            data  = pandas.DataFrame.from_dict(req.json()['data'] )
+        except:
+            self.window.geometry("790x200")
+            return
         nbL = 2
         for i in data.values:
             nbC = 0
@@ -31,6 +33,7 @@ class User :
                 nbC += 1
             Button(self.window,text='Reserver',command= lambda ID=i[0] : self.Reservation( ID )).grid(row=nbL,column=nbC,sticky=W,pady=4)
             nbL += 1
+        self.window.geometry("790x"+str((nbL-1)*40+50))# taille dynamique
     def List_Deja_Reserver(self):
         self.Clean_Screen()
         Label(self.window, text="Liste des Vols Reserver").grid(row=0 , column=0)
@@ -41,10 +44,11 @@ class User :
         Input = {'Id_User': self.Index_User}
         req = requests.post(self.url+"info", data = Input)
         if( req.status_code == 404 ):
-            print( "count" )
+            print( " Not Found" )
         try:
             data  = pandas.DataFrame.from_dict(req.json()['data'] )
         except : 
+            self.window.geometry("790x200")
             return
         nbL = 2  
         for i in data.values:
@@ -54,6 +58,7 @@ class User :
                 nbC += 1
             Button(self.window,text='Annuler',command= lambda ID=i[0] : self.Annuler_Reservation( ID )).grid(row=nbL,column=nbC+1,sticky=W,pady=4)
             nbL += 1
+        self.window.geometry("790x"+str((nbL-1)*40+50))
     def Reservation(self, INDX):
         Input = {'Mode':'Reserve','Id_User': self.Index_User ,'Id_Vol':INDX }
         req = requests.post(self.url+"billets", data = Input)
