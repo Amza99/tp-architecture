26bdf4a4935a6c101285aaca8e9522e315450013
AmZa <32337304+Amza99@users.noreply.github.com>
2020-12-10 12:46:08 +0100

    100% Last Update

--- IA-MaghrebUnited/Projet/Code/menu.py
+++ IA-MaghrebUnited/Projet/Code/menu.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import tkinter as tk
+from tkinter import Button , Label , W , Tk , Entry
 import requests
 from User_Interface import User
+from Admin_Interface import Admin
+
+Admin_Log = {"Nom":'admin',"Prenom":"admin"} # Login Administrateur  
+
 class Menu :
     def __init__(self , fenetre ):
         print(" Menu ")
@@ -12,37 +16,41 @@ class Menu :
         self.window.title(' Maghreb_United')
         self.Create_Button()
         self.Create_Entry()
-        tk.Label(self.window, text=""" Projet Architecture Distribue 
+        Label(self.window, text=""" Projet Architecture Distribue 
                  Groupe : Maghreb United 
                  """).grid(row=0 , column=0)
-        tk.Label(self.window, text=""" 
+        Label(self.window, text=""" 
                  Membres :
                      Hamza BOUKHRISS / Mohamed AMTALSSI
                      Ibrahim LABIOD /  Oumaima BOUYAHY""").grid(row=4 , column=0)
 
     def Create_Button(self):
-        tk.Button(self.window,text="  Connection/Inscription ",
-          command=self.Connection , fg='red').grid(row=3,column=1,sticky=tk.W,pady=4)
+        Button(self.window,text="  Connection/Inscription ",
+          command=self.Connection , fg='red').grid(row=3,column=1,sticky=W,pady=4)
     def Create_Entry(self):
-        self.nom = tk.Entry(self.window , width=10)
-        self.prenom = tk.Entry(self.window , width=10)
-        tk.Label(self.window, text=" Nom =>").grid(row=1 , column=0)
+        self.nom = Entry(self.window , width=10)
+        self.prenom = Entry(self.window , width=10)
+        Label(self.window, text=" Nom =>").grid(row=1 , column=0)
         self.nom.grid(row=1 , column=1)
-        tk.Label(self.window, text=" Prenom =>").grid(row=2 , column=0)
+        Label(self.window, text=" Prenom =>").grid(row=2 , column=0)
         self.prenom.grid(row=2 , column=1)
 
     def Connection(self):
         Input = {'Nom': self.nom.get(),'Prenom':self.prenom.get()}
+        if Input["Nom"] == Admin_Log["Nom"] and Input["Prenom"] == Admin_Log["Prenom"]:
+            new = self.Create_AND_Destroy()
+            Admin(new)
+            return
         req = requests.post(self.url, data = Input)
         INDEX = req.json()['Index']
         new = self.Create_AND_Destroy()
         User(new,INDEX)
 
     def Create_AND_Destroy(self):
-        new_window = tk.Tk()
+        new_window = Tk()
         self.window.destroy()
         return new_window
 
-window = tk.Tk()
+window = Tk()
 Menu(window)
 window.mainloop()
